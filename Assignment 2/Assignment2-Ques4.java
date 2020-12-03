1abf2f1be5efa063a0c0c46a300025f3f9af698b
pareshgupta6700 <68894301+pareshgupta6700@users.noreply.github.com>
2020-12-04 03:12:50 +0530

    Update Assignment2-Ques4.java

--- Assignment 2/Assignment2-Ques4.java
+++ Assignment 2/Assignment2-Ques4.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int sum=0, i=1;
         for(; sum<Integer.MAX_VALUE && i<Integer.MAX_VALUE; i++) {
-            if(sum>=Integer.MAX_VALUE-1)
+            if(sum>=Integer.MAX_VALUE-i)
                 break;
             sum+=i;
             if(sum==i*i)
