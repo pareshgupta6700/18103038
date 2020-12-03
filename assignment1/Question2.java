6517d0d056cb2a672b97f5e4032cdebd41fd1b81
pareshgupta6700 <68894301+pareshgupta6700@users.noreply.github.com>
2020-12-04 02:43:47 +0530

    Update Question2.java

--- assignment1/Question2.java
+++ assignment1/Question2.java
@@ -1,54 +1,27 @@
 import java.util.Scanner;
-public class Question2{
-	public static void main(String[] args) {
-		System.out.println("Enter the string:");
-		Scanner scan=new Scanner(System.in);
-		String p;
-		p=scan.nextLine();
-		System.out.println("Enter the size of replacement vector:");
-		int n=scan.nextInt();
-		int x=0;
-		String[] s=new String[500];
-		String[] s1=new String[n];
-		String[] s2=new String[n];
-		String temp="";
-		java.util.HashMap<String, Integer> mark = new java.util.HashMap<String, Integer>();
-		for(int i=0;i<n;i++){
-	        System.out.println("Enter word to be replaced: ");
-	        s1[i]=scan.next();
-	        s2[i]=String.valueOf(s1[i].charAt(0)); 
-	        for(int r=0;r<s1[i].length()-1; r++) {
-	        	s2[i]=s2[i]+'*';
-	        }
-	        mark.put(s1[i],i+1);}
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-	        int k=p.length();
-
-	        for(int i=0;i<k;i++){
-	        int j=i;
-	        temp="";
-
-	        while((j<k)&&(p.charAt(j)!=' ')){
-	        temp=temp+p.charAt(j);
-	        j++;}
-
-	        if(temp!=" ")
-	        i=j;
-
-	        s[x]=temp;
-	        x++;}
-
-	        for(int i=0;i<x;i++){
-
-	        if(mark.containsKey(s[i])){
-	        int z=mark.get(s[i]);
-	        s[i]=s2[z-1];}}
-
-	        for(int i=0;i<x;i++)
-	        System.out.print(s[i]+" ");
-
-	        System.out.print("\n");
-		
-		
-	}
+public class question2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Paragraph: ");
+        String paragraph = input.nextLine();
+        System.out.print("Enter strings: ");
+        String[] wordsToBeReplaced = input.nextLine().split(" ");
+        input.close();
+        for (String word : wordsToBeReplaced) {
+            Pattern p = Pattern.compile("\\b" + word + "\\b");
+            Matcher m = p.matcher(paragraph);
+            if (m.find()) {
+                StringBuilder newString = new StringBuilder();
+                newString.append(word.charAt(0));
+                for (int i = 1; i < word.length(); i++) {
+                    newString.append('*');
+                }
+                paragraph = m.replaceAll(newString.toString());
+            }
+        }
+        System.out.println("Updated Paragraph: " + paragraph);
+    }
 }
